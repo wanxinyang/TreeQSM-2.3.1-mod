37cee833245d08e718f235c405814e34031becd0
wanxinyang <w.yang.17@ucl.ac.uk>
2026-02-09 12:05:34 +0000

    Update input paths in genInput_batch.sh and genInput_singleTree.sh for consistency and clarity

--- scripts/genInput_batch.sh
+++ scripts/genInput_batch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-QSM="/data/TLS2/tools/qsm/TreeQSM-2.3.1-mod-matlab/python/"
-IN_DIR="/data/TLS2/uk/epping-pollards/demo/clouds/float64"
-OUT_DIR="/data/TLS2/uk/epping-pollards/demo/models/param"
-RDIR="/data/TLS2/uk/epping-pollards/demo/models/results"
+TREEQSM="/data/TLS2/tools/qsm/TreeQSM-2.3.1-mod-matlab/python/"
+CLOUD_DIR="/PATH/TO/clouds/float64"
+PARAMS_DIR="/PATH/TO/models/intermediate/params"
+QSM_CANDIDATE_DIR="/PATH/TO/models/qsm_candidates"
 
-for ply_file in "${IN_DIR}"/*.ply; do
+for ply_file in "${CLOUD_DIR}"/*.ply; do
     base_name=$(basename "${ply_file}" .ply)
     # out_file="${OUT_DIR}/${base_name}_param"
 
-    python "${QSM}/generate_inputs-updated-matlab.py" \
+    python "${TREEQSM}/generate_inputs-updated-matlab.py" \
         -i "${ply_file}" \
-        -o "${OUT_DIR}/${base_name}" \
-        -rdir "${RDIR}/${base_name}/" \
+        -o "${PARAMS_DIR}/${base_name}" \
+        -rdir "${QSM_CANDIDATE_DIR}/${base_name}/" \
         --patchdiam1 0.2 0.25 0.3 \
         --patchdiam2min 0.05 0.1 0.15 \
         --patchdiam2max 0.15 0.2 0.25 \
--- scripts/genInput_singleTree.sh
+++ scripts/genInput_singleTree.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-QSM="/data/TLS2/tools/qsm/TreeQSM-2.3.1-mod-matlab/python/"
+TREEQSM="/data/TLS2/tools/qsm/TreeQSM-2.3.1-mod-matlab/python/"
+CLOUD_DIR="/PATH/TO/clouds/float64/TreeX.ply"
+PARAMS_DIR="/PATH/TO/models/intermediate/params/TreeX"
+QSM_CANDIDATE_DIR="/PATH/TO/models/qsm_candidates/TreeX"
 
-python "${QSM}/generate_inputs-updated-matlab.py" \
-    -i /data/TLS2/uk/epping-pollards/demo/clouds/float64/Scan_14_extracted_float64.ply \
-    -o /data/TLS2/uk/epping-pollards/demo/models/param/Scan_14_param \
-    -rdir /data/TLS2/uk/epping-pollards/demo/models/results/Scan_14/ \
+python "${TREEQSM}/generate_inputs-updated-matlab.py" \
+    -i "${CLOUD_DIR}" \
+    -o "${PARAMS_DIR}" \
+    -rdir "${QSM_CANDIDATE_DIR}" \
     --patchdiam1 0.2 0.25 0.3 \
     --patchdiam2min 0.05 0.1 0.15 \
     --patchdiam2max 0.15 0.2 0.25 \
